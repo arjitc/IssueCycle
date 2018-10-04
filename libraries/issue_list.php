fdbb8335a3fffc04950b7cf89d032db3215d9fac
Arjit Chaudhary <arjit.c@gmail.com>
2018-10-04 19:47:20 +0530

    [PATCH] Update UI

--- libraries/issue_list.php
+++ libraries/issue_list.php
@@ -16,6 +16,7 @@ function general_issue_list($limit, $issue_status, $issue_status2=NULL) {
 			$issue_status = $row["issue_status"];
 			$issue_project_key = get_issue_project_key_from_issue_id($row['id']);
 			$issue_open_since = humanTiming(get_issue_open_timestamp($row['id']));
+			$issue_project_name = get_project_name_from_project_id($row['issue_project_id']);
 			if(get_issue_status($id) == "Done") {
 				echo "<a href='issue.php?id=$id' class='list-group-item list-group-item-action flex-column align-items-start issue-done'>";
 			} else {
@@ -24,17 +25,18 @@ function general_issue_list($limit, $issue_status, $issue_status2=NULL) {
 			echo "<div class='d-flex w-100 justify-content-between'>";
 			echo "<h5 class='font-weight-normal'>";
 			if(get_issue_status($id) == "Done") {
-				echo "<span class='badge badge-success'>Done</span> [$issue_project_key-$id] $issue_subject";
+				echo "<span class='badge badge-success'>Done</span> $issue_subject";
 			} else if(get_issue_status($id) == "In Progress") {
-				echo "<span class='badge badge-info'>In Progress</span> [$issue_project_key-$id] $issue_subject";
+				echo "<span class='badge badge-info'>In Progress</span> $issue_subject";
 			} else if(get_issue_status($id) == "On Hold") {
-				echo "<span class='badge badge-dark'>On Hold</span> [$issue_project_key-$id] $issue_subject";
+				echo "<span class='badge badge-dark'>On Hold</span> $issue_subject";
 			} else {
-				echo "<span class='badge badge-primary'>Open</span> [$issue_project_key-$id] $issue_subject";
+				echo "<span class='badge badge-primary'>Open</span> $issue_subject";
 			}
 			echo "</h5>";
 			echo "<small>$issue_open_since ago</small>";
 			echo "</div>";
+			echo "<span class='badge badge-pill badge-secondary'>$issue_project_name</span> ";
 			if(empty(get_issue_members($row['id']))) {
 				echo "<small>Assignee(s): None";
 				echo " | <i class='fal fa-comments'></i> ". get_issue_comment_count($row['id'])." Comments";
@@ -63,9 +65,9 @@ function assigned_issue_list($limit, $user_id, $issue_status, $issue_status2=NUL
 	include realpath(dirname(__FILE__)).'/../config/db.php';
 	include_once realpath(dirname(__FILE__)).'../libraries/general.php';
 	if($issue_status2 != NULL) {
-		$sql ="SELECT issue_members.issue_id, issues.issue_status FROM issue_members JOIN issues ON issue_members.issue_id = issues.id WHERE issue_members.issue_member_id='$user_id' AND issues.issue_status='$issue_status' OR issues.issue_status='$issue_status2' ORDER BY issue_members.issue_id LIMIT $limit";
+		$sql ="SELECT issue_members.issue_id, issues.issue_status, issues.issue_project_id FROM issue_members JOIN issues ON issue_members.issue_id = issues.id WHERE issue_members.issue_member_id='$user_id' AND issues.issue_status='$issue_status' OR issues.issue_status='$issue_status2' ORDER BY issue_members.issue_id LIMIT $limit";
 	} else {
-		$sql ="SELECT issue_members.issue_id, issues.issue_status FROM issue_members JOIN issues ON issue_members.issue_id = issues.id WHERE issue_members.issue_member_id='$user_id' AND issues.issue_status='$issue_status' ORDER BY issue_members.issue_id LIMIT $limit";
+		$sql ="SELECT issue_members.issue_id, issues.issue_status, issues.issue_project_id FROM issue_members JOIN issues ON issue_members.issue_id = issues.id WHERE issue_members.issue_member_id='$user_id' AND issues.issue_status='$issue_status' ORDER BY issue_members.issue_id LIMIT $limit";
 	}
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
@@ -75,6 +77,7 @@ function assigned_issue_list($limit, $user_id, $issue_status, $issue_status2=NUL
 			$issue_subject = get_issue_subject($id);
 			$issue_project_key = get_issue_project_key_from_issue_id($id);
 			$issue_open_since = humanTiming(get_issue_open_timestamp($id));
+			$issue_project_name = get_project_name_from_project_id($row['issue_project_id']);
 			if(get_issue_status($id) == "Done") {
 				echo "<a href='issue.php?id=$id' class='list-group-item list-group-item-action flex-column align-items-start issue-done'>";
 			} else {
@@ -83,17 +86,18 @@ function assigned_issue_list($limit, $user_id, $issue_status, $issue_status2=NUL
 			echo "<div class='d-flex w-100 justify-content-between'>";
 			echo "<h5 class='font-weight-normal'>";
 			if(get_issue_status($id) == "Done") {
-				echo "<span class='badge badge-success'>Done</span> [$issue_project_key-$id] $issue_subject";
+				echo "<span class='badge badge-success'>Done</span> $issue_subject";
 			} else if(get_issue_status($id) == "In Progress") {
-				echo "<span class='badge badge-info'>In Progress</span> [$issue_project_key-$id] $issue_subject";
+				echo "<span class='badge badge-info'>In Progress</span> $issue_subject";
 			} else if(get_issue_status($id) == "On Hold") {
-				echo "<span class='badge badge-dark'>On Hold</span> [$issue_project_key-$id] $issue_subject";
+				echo "<span class='badge badge-dark'>On Hold</span> $issue_subject";
 			} else {
-				echo "<span class='badge badge-primary'>Open</span> [$issue_project_key-$id] $issue_subject";
+				echo "<span class='badge badge-primary'>Open</span> $issue_subject";
 			}
 			echo "</h5>";
 			echo "<small>$issue_open_since ago</small>";
 			echo "</div>";
+			echo "<span class='badge badge-pill badge-secondary'>$issue_project_name</span> ";
 			if(empty(get_issue_members($row['issue_id']))) {
 				echo "<small>Assignee(s): None";
 				echo " | <i class='fal fa-comments'></i> ". get_issue_comment_count($row['id'])." Comments";
